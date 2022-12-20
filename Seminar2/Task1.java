1e065942d9e79408b4b22bb852e734e76d812d18
Sergei Eremkin <ffr@list.ru>
2022-12-20 22:20:45 +0300

    Добавил задание со 2 семинара

---
+++ Seminar2/Task1.java
@@ -0,0 +1,33 @@
+package Seminar2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // Обработайте возможные исключительные ситуации.
+        // "Битые" значения в исходном массиве считайте нулями.
+        // Можно внести в вод правки, которые считаете необходимыми
+        String[][] arr = { { null, "0", "d" }, { "1", "0", "2" }, { "ш", "0", "2" } };
+        System.out.println(sum2d(arr));
+    }
+
+    public static int sum2d(String[][] arr) {
+        int sum = 0;
+        int val = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr[i].length; j++) {
+
+                try {
+                    val = Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    val = 0;
+                }
+                if (arr[i][j] == null) {
+                    val = 0;
+                }
+
+                sum += val;
+            }
+        }
+        return sum;
+    }
+}
---
+++ Seminar2/Task2.java
@@ -0,0 +1,80 @@
+package Seminar2;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.plaf.basic.BasicScrollPaneUI.HSBChangeListener;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        HashMap<String, String> map = readFromFile();
+        showMap(map);
+
+        // for (Map.Entry entry : map.entrySet()) {
+
+        // System.out.println("Key: " + entry.getKey() + " Value: "
+        // + entry.getValue());
+
+        // }
+
+    }
+
+    public static HashMap<String, String> readFromFile() {
+        HashMap<String, String> map = new HashMap<>();
+        String line = "";
+
+        try {
+            FileReader reader = new FileReader("Seminar2/data.txt");
+            int c;
+            while ((c = reader.read()) != -1) {
+
+                line += (char) c;
+
+            }
+            // System.out.print(line);
+            String[] lines = line.split("\n");
+            for (int i = 0; i < lines.length; i++) {
+                String[] x = lines[i].split("=");
+                map.put(x[0], x[1].strip());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return map;
+    }
+
+    public static void showMap(HashMap<String, String> map) {
+        for (Map.Entry entry : map.entrySet()) {
+            findChar(entry);
+        }
+
+    }
+
+    public static void findChar(Map.Entry<String, String> entry) {
+
+        if (entry.getValue().equals("?") || entry.getValue().equals("?\n")) {
+            System.out.println(String.valueOf(entry.getKey().length()));
+        }
+
+    }
+
+    public static void writeToFile(HashMap<String, String> map) {
+        try {
+            FileWriter writer = new FileWriter("Seminar2/data2.txt" );
+            for (Map.Entry entry : map.entrySet()) {
+                writer.append(entry.getKey() + "=" + entry.getValue());
+                // System.out.println("Key: " + entry.getKey() + " Value: "
+                // + entry.getValue());
+
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
